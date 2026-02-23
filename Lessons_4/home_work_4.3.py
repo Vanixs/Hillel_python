f5b50e2b72c88f0057cf73bd20e2a199beafd101
Vanix16 <vanix1604@gamil.com>
2026-02-23 19:52:17 +0200

    initial commit

---
+++ Lessons_4/home_work_4.3.py
@@ -0,0 +1,7 @@
+import random
+
+original_list = [random.randint(1, 10) for i in range(random.randint(3, 10))]
+
+result_list = [original_list[0], original_list[2], original_list[-2]]
+
+print(original_list, "==" , result_list)
