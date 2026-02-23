5b415b1c9f0a8fcd46a260f8cd67194c01a1058c
Vanix16 <vanix1604@gamil.com>
2026-02-23 14:40:17 +0200

    initial commit

--- Lessons_3/home_work_3.1.py
+++ Lessons_3/home_work_3.1.py
@@ -1,25 +1,20 @@
 number_1 = int(input("1_st number:"))
 number_2 = int(input("2_nd number:"))
 
-print("+_print_1:")
-print("-_print_2:")
-print("/_print_3:")
-print("*_print_4:")
-
 actions = input("Write your actions:")
-if actions == "1":
+if actions == "+":
     answer = number_1 + number_2
     print(answer)
-elif actions == "2":
+elif actions == "-":
     answer = number_1 - number_2
     print(answer)
-elif actions == "3":
+elif actions == "/":
     if number_2 == 0:
         print("wrong!")
     else:
         answer = number_1 // number_2
         print(answer)
-elif actions == "4":
+elif actions == "*":
     answer = number_1 * number_2
     print(answer)
 else:
