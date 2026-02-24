5e726b9b4425344c565b4e7862d90f668491d1ae
Vanix16 <vanix1604@gamil.com>
2026-02-24 10:07:47 +0200

    initial commit

--- Lessons_4/home_work_4.1.py
+++ Lessons_4/home_work_4.1.py
@@ -1,6 +1,6 @@
 numbers = [1, 0, 13, 0, 0, 0, 5]
 
-non_zeros = [x for x in numbers if x != 0]
+non_zeros = [i for i in numbers if i != 0]
 zeros_count = numbers.count(0)
 result = non_zeros + [0] * zeros_count
 
