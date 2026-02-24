e68495b1b3dd6cbc4c1cd60d47fe510aa5323db7
Vanix16 <vanix1604@gamil.com>
2026-02-24 10:06:40 +0200

    initial commit

--- Lessons_4/home_work_4.2.py
+++ Lessons_4/home_work_4.2.py
@@ -1,4 +1,4 @@
 cases = [[0, 1, 7, 2, 4, 8], [1, 3, 5], [6], []]
 
-for lst in cases:
-    print(sum(lst[::2]) * lst[-1] if lst else 0)
+for list in cases:
+    print(sum(list[::2]) * list[-1] if list else 0)
