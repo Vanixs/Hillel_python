451ca54fed7a5db2f4d3c25f2c1a7e3da9fee990
Vanix16 <vanix1604@gamil.com>
2026-04-20 19:31:13 +0300

    initial commit

---
+++ Lessons_15/15.1.py
@@ -0,0 +1,64 @@
+class GroupLimitError(Exception):
+    def __init__(self, message="У групі не може бути більше 10 студентів"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.gender}, {self.age} years old"
+
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"{super().__str__()}, Record Book: {self.record_book}"
+
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= 10:
+            raise GroupLimitError()
+        self.group.add(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number:{self.number}\n{all_students}'
+
+
+gr = Group('PD1')
+
+try:
+    for i in range(11):
+        st = Student('Male', 18 + i, f'Name{i}', f'Lastname{i}', f'RB{i}')
+        gr.add_student(st)
+        print(f"Added: {st.last_name}")
+except GroupLimitError as e:
+    print(f"Помилка: {e}")
+
+print("\nFinal Group State:")
+print(gr)
